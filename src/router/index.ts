db8d358460b5ae6e4974b0d471ad0d7c217faafc
yutounun <geek.yuto@gmail.com>
2023-01-28 16:54:02 +0900

    Add titles on each page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,23 @@
 import { createRouter, createWebHistory } from "vue-router";
 
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: () => import("@/pages/HomeView.vue"),
+    meta: {
+      title: "HOME画面",
+    },
+  },
+];
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: "/",
-      name: "home",
-      component: () => import("../pages/HomeView.vue"),
-    },
-  ],
+  routes,
+});
+
+router.afterEach((to) => {
+  document.title = to.meta.title as string;
 });
 
 export default router;
