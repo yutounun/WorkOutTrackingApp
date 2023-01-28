3890caef61fad4aea22c0f56620d1909a4b02d18
yutounun <geek.yuto@gmail.com>
2023-01-28 16:58:03 +0900

    change config to check local on mobile as well

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,13 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: true,
+    port: 3000,
+    watch: {
+      usePolling: true,
+    },
+  },
   plugins: [vue()],
   resolve: {
     alias: {
