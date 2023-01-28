086f1490b3af75594777b0cc1ca6aa1bdfe0878f
yutounun <geek.yuto@gmail.com>
2023-01-28 14:22:02 +0900

    Solve all lint errors

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../pages/HomeView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
+/* eslint-env node */
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,vue}"],
   theme: {
