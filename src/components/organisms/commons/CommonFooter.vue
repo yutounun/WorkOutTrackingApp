cd254758b7871e597e86d1699c379c2636eaef00
yutounun <geek.yuto@gmail.com>
2023-01-29 15:03:38 +0900

    fix: Change the container of CommonFooter

--- src/components/organisms/commons/CommonFooter.vue
+++ src/components/organisms/commons/CommonFooter.vue
@@ -2,13 +2,6 @@
 import { useRouter, type RouteLocationRaw } from "vue-router";
 
 const router = useRouter();
-const props = defineProps({
-  /** Select From which page you want to call this method. */
-  home: Boolean,
-  food: Boolean,
-  menu: Boolean,
-  profile: Boolean,
-});
 
 const transition = (to: RouteLocationRaw) => {
   router.push(to);
@@ -17,70 +10,31 @@ const transition = (to: RouteLocationRaw) => {
 
 <template>
   <footer
-    class="mx-4 h-20 flex items-center rounded-xl mb-5 flex-wrap flex-row fixed inset-x-0 bottom-0 drop-shadow-lg"
-    style="background-image: url('/icons/Container.svg')"
+    class="mx-4 h-20 flex items-center rounded-xl mb-5 flex-wrap flex-row bg-white fixed inset-x-0 bottom-0 drop-shadow-lg border border-gray-2"
   >
-    <div
-      v-if="props.home"
-      class="w-1/5 text-center"
-      @click="transition('/home')"
-    >
-      <img src="/icons/SelectedHome.svg" class="m-auto" alt="" />
-      <span class="text-xxs text-black block mb-1 font-bold"> HOME </span>
-    </div>
-    <div v-else class="w-1/5 text-center" @click="transition('/home')">
+    <div class="w-1/5 text-center">
       <img src="/icons/Home.svg" class="m-auto" alt="" />
-      <span class="text-xxs text-black block mb-1 font-bold"> HOME </span>
+      <span class="text-xxs text-black block mb-1 font-mono"> HOME </span>
     </div>
-
-    <div
-      v-if="props.food"
-      class="w-1/5 text-center"
-      @click="transition('/food')"
-    >
-      <img src="/icons/SelectedFood.svg" class="m-auto" alt="" />
-      <span class="text-xxs text-black block mb-1 font-bold"> FOOD </span>
-    </div>
-    <div v-else class="w-1/5 text-center" @click="transition('/food')">
+    <div class="w-1/5 text-center">
       <img src="/icons/Food.svg" class="m-auto" alt="" />
-      <span class="text-xxs text-black block mb-1 font-bold"> FOOD </span>
+      <span class="text-xxs text-black block mb-1 font-mono"> FOOD </span>
     </div>
-
-    <!-- + icon -->
     <img
       src="/icons/LargeAddBtn.svg"
       alt=""
-      class="w-1/5 absolute bottom-5 left-[157px]"
+      class="w-1/5 absolute bottom-5 left-40"
     />
-
     <div class="w-1/5 text-center">
       <span class="hidden"> </span>
     </div>
-
-    <div
-      v-if="props.menu"
-      class="w-1/5 text-center"
-      @click="transition('/workout')"
-    >
-      <img src="/icons/SelectedMenu.svg" class="m-auto" alt="" />
-      <span class="text-xxs text-black block mb-1 font-bold"> MENU </span>
-    </div>
-    <div v-else class="w-1/5 text-center" @click="transition('/workout')">
+    <div class="w-1/5 text-center">
       <img src="/icons/Menu.svg" class="m-auto" alt="" />
-      <span class="text-xxs text-black block mb-1 font-bold"> MENU </span>
+      <span class="text-xxs text-black block mb-1 font-mono"> MENU </span>
     </div>
-
-    <div
-      v-if="props.profile"
-      class="w-1/5 text-center"
-      @click="transition('/profile')"
-    >
-      <img src="/icons/SelectedProfile.svg" class="m-auto" alt="" />
-      <span class="text-xxs text-black block mb-1 font-bold"> PROFILE </span>
-    </div>
-    <div v-else class="w-1/5 text-center" @click="transition('/profile')">
+    <div class="w-1/5 text-center">
       <img src="/icons/Profile.svg" class="m-auto" alt="" />
-      <span class="text-xxs text-black block mb-1 font-bold"> PROFILE </span>
+      <span class="text-xxs text-black block mb-1 font-mono"> PROFILE </span>
     </div>
   </footer>
 </template>
