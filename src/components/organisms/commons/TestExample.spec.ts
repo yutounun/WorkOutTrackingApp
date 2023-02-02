dcd5ec6e7f1a4a15c75b5d53215833b4141a9fb6
yutounun <geek.yuto@gmail.com>
2023-02-02 09:13:35 +0900

    feat: add vue-testing-library and example for that

---
+++ src/components/organisms/commons/TestExample.spec.ts
@@ -0,0 +1,21 @@
+import { it } from "vitest";
+
+import { render, fireEvent } from "@testing-library/vue";
+import Component from "./TestExample.vue";
+
+it("increments value on click", async () => {
+  // The render method returns a collection of utilities to query your component.
+  const { getByText } = render(Component);
+
+  // getByText returns the first matching node for the provided text, and
+  // throws an error if no elements match or if more than one match is found.
+  getByText("Times clicked: 0");
+
+  const button = getByText("increment");
+
+  // Dispatch a native click event to our button element.
+  await fireEvent.click(button);
+  await fireEvent.click(button);
+
+  getByText("Times clicked: 2");
+});
