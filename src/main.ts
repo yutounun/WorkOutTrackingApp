adf9911c47b0452ccf482f507f4fa44a592d5b7b
yutounun <geek.yuto@gmail.com>
2023-02-05 19:53:23 +0900

    fix: pwa

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { createPinia } from "pinia";
 
 import App from "./App.vue";
 import router from "./router";
+import { registerSW } from "virtual:pwa-register";
+
+registerSW();
 
 import "./assets/main.css";
 
