3b4018a94217038b30352a15ade733dc2910fd07
yutounun <geek.yuto@gmail.com>
2023-02-13 22:19:26 +0900

    :sparkles: Get and set info about weight and fat on chart page

--- src/pages/GraphPage.vue
+++ src/pages/GraphPage.vue
@@ -1,45 +1,58 @@
 <script setup lang="ts">
+import { db } from "@/firebase/init";
 import CommonGraph from "@/components/organisms/commons/CommonGraph.vue";
 import Goal from "@/components/organisms/chart/ActivityGoals.vue";
 import Header from "@/components/organisms/commons/CommonHeader.vue";
 import Footer from "@/components/organisms/commons/CommonFooter.vue";
+import { useProfileStore } from "@/stores/profile";
+import { doc, getDoc } from "@firebase/firestore";
+import { ref } from "vue";
 
-const foodDataSet = [
-  {
-    label: "Protein",
-    backgroundColor: "#918EF4",
-    data: [40, 39, 10, 40, 39, 80, 300],
-  },
-  {
-    label: "Fat",
-    backgroundColor: "#141B41",
-    data: [20, 19, 40, 10, 60, 10, 40],
-  },
-  {
-    label: "Carbo",
-    backgroundColor: "#98B9F2",
-    data: [20, 19, 30, 80, 20, 80, 10],
-  },
-];
+const email = ref("");
+const goalData = ref({});
 
-const weightDataSet = [
-  {
-    label: "Weight",
-    backgroundColor: "#918EF4",
-    data: [40, 39, 10, 40, 39, 80, 300],
-  },
-];
+/** Get info about user such as weight */
+const getProfile = async () => {
+  email.value = await useProfileStore().email;
 
-const goalData = {
-  weight: {
-    current: 54,
-    goal: 39,
-  },
-  bodyFat: {
-    current: 22,
-    goal: 15,
-  },
+  const docRef = doc(db, "users", email.value);
+  const docSnap = await getDoc(docRef);
+  if (docSnap.exists()) {
+    const profile = docSnap.data();
+    goalData.value = {
+      weight: {
+        current: profile.currentWeight,
+        goal: profile.idealWeight,
+      },
+      bodyFat: {
+        current: profile.currentBodyFat,
+        goal: profile.idealBodyFat,
+      },
+    };
+  } else {
+    // doc.data() will be undefined in this case
+    console.log("No such document!");
+  }
 };
+
+// created
+getProfile();
+
+// const foodDataSet = [
+//   {
+//     label: "Carbo",
+//     backgroundColor: "#98B9F2",
+//     data: [20, 19, 30, 80, 20, 80, 10],
+//   },
+// ];
+
+// const weightDataSet = [
+//   {
+//     label: "Weight",
+//     backgroundColor: "#918EF4",
+//     data: [40, 39, 10, 40, 39, 80, 300],
+//   },
+// ];
 </script>
 <template>
   <Header title="Your recent activity" />
